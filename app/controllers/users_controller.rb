ee01e1043fa64c28275c150879e92f9bcf50a612
EC2 Default User <ec2-user@ip-172-31-45-56.ap-northeast-1.compute.internal>
2020-03-20 10:29:10 +0000

    5_e2_finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
- 
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :logged_in_user, only: [:show, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
 
   def new
     @user = User.new
@@ -16,18 +19,71 @@ class UsersController < ApplicationController
     end
   end
   
+  def index
+    @users = User.paginate(page: params[:page], per_page: 20)
+  end
+  
   def show
-    @user = User.find(params[:id])
+    
   end
   
   def edit
-    @user = User.find(params[:id])
+  
+  end
+  
+  def update
+   
+    if @user.update_attributes(user_params)
+      flash[:success] = "タスク情報を更新しました。"
+      redirect_to user_url
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @user.destroy
+    flash[:success] = "#{@user.name}のデータを削除しました。"
+    redirect_to users_url
   end
   
   
   private
   
+  
+    def set_user
+      @user = User.find(params[:id])
+    end
+    
+    
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
+    
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "ログインしてください。"
+        redirect_to login_url
+      end
+    end
+    
+    
+    def correct_user
+      redirect_to(root_url) unless current_user?(@user)
+    end
+    
+    def admin_user
+      redirect_to root_url unless current_user.admin?
+    end
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,4 +3,24 @@
 User.create!( name: "Sample User",
               email: "sample@email.com",
               password: "password",
-              password_confirmation: "password")
+              password_confirmation: "password",
+              admin: true)
+                
+100.times do |n|
+  name = Faker::Name.name
+  email = "sample-#{n+1}@email.com"
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password)
+end                 
+                 
+@users = User.order(:created_at).take(3)
+50.times do |n|
+  task_name =Faker::Lorem.sentence(2)
+  task_discription = Faker::Lorem.sentence(5)
+  @users.each { |user| user.tasks.create!(name: task_name, discription: task_discription) }
+end               
+
+
